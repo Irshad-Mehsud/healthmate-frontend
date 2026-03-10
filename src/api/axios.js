bb71a6e446b92da3c503dbd4491126bd353f2fbe
Irshad-Mehsud <irshadahmad19059@gmail.com>
2026-03-10 12:26:30 +0500

    Adding Different Sections

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,13 +5,13 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://healthmate-backend-three.vercel.app/"
+      ? "https://healthmate-backend-three.vercel.app/api/"
       : "http://localhost:5000/api/",
-  withCredentials: true, // required for HTTP-only cookies
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
-  timeout: 10000, // optional: request timeout (10s)
+  timeout: 10000,
 });
 
 // ============================
